1d71f195a09084833f5330942512ff32c69622f7
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-16 10:40:16 -0500

    Done.

--- index.js
+++ index.js
@@ -10,87 +10,95 @@
  */
 
 let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+  let eligibleDates = this.timeInEvents.map(function (e) {
+    return e.date;
+  });
 
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+  let payable = eligibleDates.reduce(
+    function (memo, d) {
+      return memo + wagesEarnedOnDate.call(this, d);
+    }.bind(this),
+    0
+  ); // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
-}
+  return payable;
+};
 
-function createEmployeeRecord(scrArray){
-    return {
+function createEmployeeRecord(scrArray) {
+  return {
     firstName: scrArray[0],
     familyName: scrArray[1],
     title: scrArray[2],
     payPerHour: scrArray[3],
     timeInEvents: [],
-    timeOutEvents: []
-    }
+    timeOutEvents: [],
+  };
 }
 
-function createEmployeeRecords(scrArray){
-    return scrArray.map(element => createEmployeeRecord(element))
+function createEmployeeRecords(scrArray) {
+  return scrArray.map((element) => createEmployeeRecord(element));
 }
 
 function createTimeInEvent(timeDate) {
-    const hour = parseInt(timeDate.split(" ")[1])
-    const date = timeDate.split(" ")[0]
-    this.timeInEvents.push({
-        type: "TimeIn",
-        hour: hour,
-        date: date,
-    })
-    return this
+  const hour = parseInt(timeDate.split(" ")[1]);
+  const date = timeDate.split(" ")[0];
+  this.timeInEvents.push({
+    type: "TimeIn",
+    hour: hour,
+    date: date,
+  });
+  return this;
 }
 
-function createTimeOutEvent(timeDate){
-    const date = timeDate.split(" ")[0]
-    const hour = parseInt(timeDate.split(" ")[1])
+function createTimeOutEvent(timeDate) {
+  const date = timeDate.split(" ")[0];
+  const hour = parseInt(timeDate.split(" ")[1]);
 
-    this.timeOutEvents.push({
-        type: "TimeOut",
-        hour: hour,
-        date: date,
-    })
-    return this
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: hour,
+    date: date,
+  });
+  return this;
 }
 
 // let cRecord = createEmployeeRecord(["Julius", "Caesar", "General", 27])
 // createTimeInEvent.call(cRecord, "2044-03-15 0900")
 // createTimeOutEvent.call(cRecord, "2044-03-15 1100")
 
-cRecord = createEmployeeRecord(["Julius", "Caesar", "General", 27])
-        createTimeInEvent.call(cRecord, "2044-03-15 0900")
-        createTimeOutEvent.call(cRecord, "2044-03-15 1100")
+// cRecord = createEmployeeRecord(["Julius", "Caesar", "General", 27]);
+// createTimeInEvent.call(cRecord, "2044-03-15 0900");
+// createTimeOutEvent.call(cRecord, "2044-03-15 1100");
 
-function hoursWorkedOnDate(date){
-    const timeIn = this.timeInEvents.find(e =>{
-        return e.date === date
-    }).hour
-    const timeOut = this.timeOutEvents.find(e => {
-        return e.date === date
-    }).hour
-    return (timeOut - timeIn) / 100
+function hoursWorkedOnDate(date) {
+  const timeIn = this.timeInEvents.find((e) => {
+    return e.date === date;
+  }).hour;
+  const timeOut = this.timeOutEvents.find((e) => {
+    return e.date === date;
+  }).hour;
+  return (timeOut - timeIn) / 100;
 }
 
-function wagesEarnedOnDate(date){
-    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+function wagesEarnedOnDate(date) {
+  return hoursWorkedOnDate.call(this, date) * this.payPerHour;
 }
 
-function payrollExpense(arrayOfEmployees) {
-    return arrayOfEmployees.map(employee => {
-            return employee.timeOutEvents.map(date => {
-                return wagesEarnedOnDate(date)
-            }).reduce((acc, curr => acc += curr))
-    })
+
+function calculatePayroll(arrayOfEmployees) {
+  return arrayOfEmployees.map((employee) => {
+    return employee.timeOutEvents
+      .map((date) => {
+        return wagesEarnedOnDate(date);
+      })
+      .reduce((acc, (curr) => (acc += curr)));
+  });
+
 }
+
 // function calculatePayroll(array){
 // }
 
-function findEmployeeByFirstName(collection, firstName){
-    return collection.find(employee => employee.firstName === firstName)
+function findEmployeeByFirstName(collection, firstName) {
+  return collection.find((employee) => employee.firstName === firstName);
 }
